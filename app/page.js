6c79db5af3a6bf6cacce1b3616ed64df10621a6b
Gustavo Peña Beltrami <gustavopb99c@gmail.com>
2024-07-31 13:33:48 -0300

    cambio de textos

--- app/page.js
+++ app/page.js
@@ -19,18 +19,13 @@ export default function HomePage() {
         {/* Nosotros */}
         <Carousel />
         <p className="w-full md:text-lg text-md mb-8">
-          Por otra parte, otro aspecto muy importante dentro de nuestra profesión, es la Administración. Y volvemos a lo simple, cobrar un alquiler mensual, emitir un recibo es tarea sencilla. Mantener el valor del Patrimonio con el paso del tiempo es el verdadero desafío. Y ahí está la clave. Desde la redacción del contrato, las garantías, la intermediación permanente llevando claridad a las partes ante dudas que generen conflictos de intereses, requiere de una profesionalidad, claridad en el conocimiento, y atención permanente. Por todo esto, a la hora de elegir una inmobiliaria, no contrates dos, tres, cuatro, quedate con una, asociate, recordá que no se trata de un cartel y nada más, todos queremos obtener el negocio, realizar la venta. Elegí la oficina con la que te sientas más cómodo. Dejanos hacer nuestro trabajo, para el que constantemente nos estamos capacitando, y con paciencia, tarde o temprano la venta se va a dar. (realidad en el negocio  inmobiliario…).
-        </p>
+          Por otra parte, otro aspecto muy importante dentro de nuestra profesión, es la Administración. Y volvemos a lo simple, cobrar un alquiler mensual, emitir un recibo es tarea sencilla. Mantener el valor del Patrimonio con el paso del tiempo es el verdadero desafío. Y ahí está la clave. Desde la redacción del contrato, las garantías, la intermediación permanente llevando claridad a las partes ante dudas que generen conflictos de intereses, requiere de una profesionalidad, claridad en el conocimiento, y atención permanente. Por todo esto, a la hora de elegir una inmobiliaria, no contrates dos, tres, cuatro, quedate con una, asociate, recordá que no se trata de un cartel y nada más, todos queremos obtener el negocio. Elegí la oficina con la que te sientas más cómodo.        </p>
         <h3 className="w-full md:text-2xl text-xl mb-10 subrayado-rojo font-bold mt-10">Misión</h3>
         <p className="w-full md:text-lg text-md mb-8">
-          En Farias Cornejo Negocios Inmobiliarios, nuestra misión es transformar cada transacción inmobiliaria en una experiencia significativa y positiva. Sabemos que cada propiedad es mucho más que un bien material; es un hogar lleno de recuerdos y emociones. Nos comprometemos a brindar un asesoramiento integral y personalizado, entendiendo las necesidades y aspiraciones de nuestros clientes. Nuestro objetivo es garantizar que cada paso del proceso sea claro, seguro y satisfactorio, creando un impacto duradero en la vida de las personas que confían en nosotros.
-        </p>
+          Nuestra misión es brindar asesoramiento integral y personalizado, entendiendo las necesidades y aspiraciones de nuestros clientes. El objetivo es garantizar que cada paso del proceso sea claro, seguro y satisfactorio, creando un impacto duradero en la vida de las personas que confían en nosotros.        </p>
         <h3 className="w-full md:text-2xl text-xl mb-10 subrayado-rojo font-bold mt-10">Visión</h3>
         <p className="w-full md:text-lg text-md mb-8">
-          Nuestra visión es ser la inmobiliaria de referencia en Sierras Chicas, reconocida por nuestra capacidad de conectar emocionalmente con nuestros clientes y por nuestro compromiso con su bienestar. Queremos liderar el mercado, no solo por la calidad de nuestros servicios, sino por la empatía y dedicación con la que abordamos cada operación. Aspiramos a ser un pilar en el crecimiento de la comunidad, ayudando a las familias a encontrar no solo una propiedad, sino un hogar donde puedan construir su futuro y crear nuevos recuerdos. Buscamos ser recordados por nuestro enfoque humano y profesional, estableciendo relaciones de confianza que perduren a lo largo del tiempo.
-
-          Fabián dirige el área comercial, aportando su experiencia y dedicación para asegurar que cada cliente tome decisiones bien informadas y alineadas con sus necesidades y sueños. Ximena, encargada de la administración de propiedades en Río Ceballos, Salsipuedes y demás localidades de Sierras Chicas, garantiza que cada propiedad bajo nuestra gestión se mantenga en perfectas condiciones, asegurando la tranquilidad de nuestros clientes.
-
+          Nuestra visión es liderar el mercado, no solo por la calidad de nuestros servicios, sino por la empatía y dedicación con la que abordamos cada operación. Aspiramos a ser un pilar en el crecimiento de la comunidad, ayudando a las familias a encontrar no solo una propiedad, sino un hogar donde puedan construir su futuro y crear nuevos recuerdos. Buscamos ser reconocidos por nuestro enfoque humano y profesional, estableciendo relaciones de confianza que perduren a lo largo del tiempo.
         </p>
 
         {/* SERVICIOS */}
@@ -52,9 +47,9 @@ export default function HomePage() {
 
         <div className="p-10 fondo-comprar-zona w-full md:py-20 rounded-lg my-10 flex md:justify-end md:pr-10 items-center">
           <div className=" flex flex-col md:w-2/4 md:items-end items-center">
-            <h3 className="text-center md:text-4xl text-xl md:text-right font-semibold mb-8 text-white md:w-full">Para la venta de propiedades</h3>
+            <h3 className="text-center md:text-4xl text-xl md:text-right font-semibold mb-8 text-white md:w-full">Venta de propiedades</h3>
             <p className="w-full md:text-lg text-sm mb-8 text-white md:text-right">
-              Ofrecemos un servicio profesional y de amplia trayectoria para la venta de propiedades. Te acompañamos desde la tasación hasta la firma del contrato, asegurando una transacción transparente y segura. Ya sea que quieras vender o comprar una propiedad, estamos aquí para asesorarte.
+            Ofrecemos un servicio profesional y de amplia trayectoria para la venta de propiedades. Te acompañamos desde la tasación hasta la firma de la escritura, asegurando una transacción transparente, confiable y segura. Ya sea que quieras vender o comprar. ¡Estamos aquí para asesorarte!.
             </p>
             <Link
               href="https://fariascorejo58.kitepropcrm.com/site/properties/sale"
--- components/Carousel.jsx
+++ components/Carousel.jsx
@@ -21,16 +21,17 @@ const Carousel = ({ interval = 3000 }) => {
       </h2>
       <div className="flex flex-wrap flex-row justify-center gap-10 mb-14 pt-4">
         <div className="bg-[#9e1c20] text-white p-6 rounded-lg shadow-lg max-w-sm text-center w-[250px] h-[250px] flex flex-col justify-between">
+          <h3 className="text-xl font-bold mb-2">Ximena Cornejo</h3>
+          <p className="mb-1">Corredor inmobiliario</p>
+          <p className="mb-1">Licenciada en publicidad</p>
+          <p>CPCPI MP 6869</p>
+        </div>
+        <div className="bg-[#9e1c20] text-white p-6 rounded-lg shadow-lg max-w-sm text-center w-[250px] h-[250px] flex flex-col justify-between">
           <h3 className="text-xl font-bold mb-2">J. Fabián Farias</h3>
           <p className="mb-1">Corredor inmobiliario</p>
           <p className="mb-1">Asesoramiento jurídico inmobiliario</p>
+          <p className="mb-1">Tasaciones</p>
           <p>CPCPI MP 4715</p>
-        </div>
-        <div className="bg-[#9e1c20] text-white p-6 rounded-lg shadow-lg max-w-sm text-center w-[250px] h-[250px] flex flex-col justify-between">
-          <h3 className="text-xl font-bold mb-2">Ximena Cornejo</h3>
-          <p className="mb-1">Licenciada en publicidad</p>
-          <p className="mb-1">Corredor inmobiliario</p>
-          <p>CPCPI MP 6869</p>
         </div>
       </div>
       <div className="carousel my-10 mx-auto overflow-hidden relative flex flex-col">
@@ -49,25 +50,31 @@ const Carousel = ({ interval = 3000 }) => {
         </div>
         <div className="md:absolute inset-0 flex flex-col justify-center items-center md:text-white py-10 md:p-10 md:bg-black md:bg-opacity-50 rounded-lg pt-16">
           <h3 className="w-full md:text-2xl lg:text-3xl text-2xl font-semibold mb-4 md:text-center letra-roja md:text-white">
-          Conozca a Farias Cornejo Negocios Inmobiliarios
+            Conozca a Farias Cornejo Negocios Inmobiliarios
           </h3>
           <h4 className="w-full md:text-xl lg:text-2xl text-lg font-semibold mb-4 md:text-center letra-roja md:text-white">
-          Inversiones Inmobiliarias en Sierras Chicas
+            Inversiones Inmobiliarias en Sierras Chicas
           </h4>
           <p className="w-full md:text-sm lg:text-lg text-md md:mb-4 lg:mb-8 md:text-center">
-          Somos Fabián Farías y Ximena Cornejo, los fundadores de Inmobiliaria Farias Cornejo. Con oficinas en Río Ceballos y Salsipuedes, nuestra pasión es ayudar a nuestros clientes a encontrar su hogar ideal o hacer la mejor inversión inmobiliaria en Sierras Chicas. Nos enorgullecemos de ofrecer un servicio personalizado basado en la confianza, la honestidad y la transparencia
+            Somos Fabián Farías y Ximena Cornejo, los fundadores de Inmobiliaria
+            Farias Cornejo. Con oficinas en Río Ceballos y Salsipuedes, nuestra
+            pasión es ayudar a nuestros clientes a encontrar su hogar ideal o
+            hacer la mejor inversión inmobiliaria en Sierras Chicas. Nos
+            enorgullecemos de ofrecer un servicio personalizado basado en la
+            confianza, la honestidad y la transparencia
           </p>
           <p className="w-full md:text-sm lg:text-lg text-md md:mb-4 lg:mb-8 md:text-center">
-          Siempre estuvimos conscientes de lo que representa elegir una Inmobiliaria para realizar una operación. También que la gran mayoría de las personas realizan compra venta de bienes inmuebles, una o dos veces en toda su vida. Son muy pocos los que realizan operaciones con frecuencia. Esto genera sensaciones de desconcierto, desconfianza, e incertidumbre a la hora de elegir.
+            Siempre estuvimos conscientes de lo que representa elegir una
+            Inmobiliaria para realizar una operación. También que la gran
+            mayoría de las personas realizan compra venta de bienes inmuebles,
+            una o dos veces en toda su vida. Son muy pocos los que concretan operaciones con frecuencia. Esto genera sensaciones de desconcierto, desconfianza, e incertidumbre a la hora de elegir.
 
           </p>
           <p className="w-full md:text-sm lg:text-lg text-md md:mb-4 lg:mb-8 md:text-center">
-          Hay muchos aspectos al momento de trabajar una propiedad, y créanme, colocar el cartel o subir la publicación en las redes, es el trabajo más simple. Hay aspectos como el Jurídico, y la capacidad de negociación, que requieren conocimiento real en el primero y experiencia en el segundo, para obtener el resultado deseado. Una operación con un final feliz.
-
+          Hay muchos aspectos al momento de trabajar una propiedad, y créanme, colocar el cartel o subir la publicación en las redes, es el trabajo más simple. Hay aspectos como el Jurídico y la capacidad de negociación, que requieren conocimiento real en el primero y experiencia en el segundo, para obtener el resultado deseado. Una operación con un final feliz.
           </p>
         </div>
       </div>
-
     </>
   );
 };
